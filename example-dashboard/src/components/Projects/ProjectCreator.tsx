244cc9733fc9ee20d8c1c421948882eca2ae8a42
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2023-01-26 00:57:22 +0300

    fix: add check for attributes in project creator

--- example-dashboard/src/components/Projects/ProjectCreator.tsx
+++ example-dashboard/src/components/Projects/ProjectCreator.tsx
@@ -2,8 +2,6 @@ import { Input, ButtonGroup, Button, Box } from "@chakra-ui/react";
 import React from "react";
 import { NewProject } from "../../types";
 import axios from "axios";
-import { useSigner } from "wagmi";
-import { ZeroWalletSigner } from 'zero-wallet-wagmi-connector';
 import useOwnerAndWebHookAttributes from "../../hooks/useOwnerAndWebHookAttributes";
 
 interface ProjectCreatorProps {
@@ -13,25 +11,24 @@ interface ProjectCreatorProps {
 export default function ProjectCreator({ onCreate }: ProjectCreatorProps) {
 
     const [name, setName] = React.useState<string>('');
-    const [allowed_origins, setAllowedOrigins] = React.useState<string[]>([]);
+    const [allowedOrigins, setAllowedOrigins] = React.useState<string[]>([]);
     const ownerAndWebHookAttributes = useOwnerAndWebHookAttributes()
 
     const handleSubmit = async () => {
-
+        if (!ownerAndWebHookAttributes) return
         const newProject: NewProject = {
             name,
-            allowed_origins,
+            allowedOrigins,
             ...ownerAndWebHookAttributes!,
         }
-        console.table(newProject)
-        await axios.post('http://localhost:3000/api/dashboard/projects', newProject);
+        await axios.post('http://localhost:3000/api/dashboard/project', newProject);
         onCreate()
     }
 
     return (
         <Box>
             <Input placeholder="Project Name" value={name} onChange={(e) => setName(e.target.value)} />
-            <Input placeholder="Allowed Origins" value={allowed_origins} onChange={(e) => setAllowedOrigins(e.target.value.split(','))} />
+            <Input placeholder="Allowed Origins" value={allowedOrigins} onChange={(e) => setAllowedOrigins(e.target.value.split(','))} />
             <ButtonGroup>
                 <Button onClick={handleSubmit}>Create</Button>
             </ButtonGroup>
