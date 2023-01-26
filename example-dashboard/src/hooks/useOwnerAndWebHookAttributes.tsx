5c516386c2354822f32c06a3f3052919a7d6fcc4
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2023-01-26 06:59:27 +0300

    fix(scwOwner): load after scw deployment

--- example-dashboard/src/hooks/useOwnerAndWebHookAttributes.tsx
+++ example-dashboard/src/hooks/useOwnerAndWebHookAttributes.tsx
@@ -1,27 +1,34 @@
-import { useSigner } from "wagmi";
+import { useContext, useEffect, useState } from 'react';
+import { useSigner } from 'wagmi';
 import { ZeroWalletSigner } from 'zero-wallet-wagmi-connector';
-import React, { useEffect } from 'react';
-import { IBase } from "../types";
-
+import { ProjectsContext } from '../../pages/_app';
+import { IBase } from '../types';
 
 export default function useOwnerAndWebHookAttributes() {
+    const { doesScwExist } = useContext(ProjectsContext)!;
     const { data: signer } = useSigner<ZeroWalletSigner>();
-    const [webHookAttributes, setWebHookAttributes] = React.useState<IBase>();
+    const [webHookAttributes, setWebHookAttributes] = useState<IBase>();
 
     useEffect(() => {
         if (!signer) return;
+        if (!doesScwExist) return;
         const getWebHookAttributes = async () => {
             const nonce = await signer.getNonce();
             setWebHookAttributes({
-                ownerScw: signer.scwAddress || '',
+                ownerScw: signer.getScwAddress() || '',
                 webHookAttributes: {
                     nonce,
-                    signedNonce: await signer.signNonce(nonce),
+                    signedNonce: await signer.signNonce(nonce)
                 }
-            })
-        }
+            });
+        };
+
         getWebHookAttributes();
-    }, [signer])
+    }, [signer, doesScwExist]);
+
+    useEffect(() => {
+        console.log(webHookAttributes);
+    }, [webHookAttributes]);
 
     return webHookAttributes;
 }
