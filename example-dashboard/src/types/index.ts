3d5e7d0084a159850681f4ac211ba9b242def199
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2023-01-26 07:01:43 +0300

    chore(types): add context types

--- example-dashboard/src/types/index.ts
+++ example-dashboard/src/types/index.ts
@@ -4,21 +4,20 @@ export interface GasTankType {
     gas_tank_id: string;
     project_id: string;
     created_at: string;
-    name: string;
     chain_id: string;
     provider_url: string;
     funding_key: string;
     whitelist: string[];
+    balance: string;
 }
 
 export interface NewGasTank extends IBase {
-    name: string;
-    chain_id: string;
-    provider_url: string;
+    chainId: number;
+    providerURL: string;
     whitelist: string[];
 }
 
-export interface ProjectType {
+export interface ProjectApiType {
     project_id: string;
     project_api_key: string;
     name: string;
@@ -36,3 +35,20 @@ export interface NewProject extends IBase {
     name: string;
     allowedOrigins: string[];
 }
+
+export type IProject = ProjectApiType & {
+    gasTanks: IGasTank[];
+};
+
+export type IGasTank = GasTankType;
+
+export interface CreateProjectEntity {
+    createNew: 'project'
+}
+
+export interface CreateGasTankEntity {
+    createNew: 'gasTank';
+    projectId: string;
+}
+
+export type EntityType = IProject | IGasTank | CreateProjectEntity | CreateGasTankEntity;
