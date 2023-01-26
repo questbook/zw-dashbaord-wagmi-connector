7cb52d3ecd8f27b7b50f75755bcb81c08b08d0d4
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2023-01-26 06:55:28 +0300

    chore: remove old projects and gas tanks viewers

--- example-dashboard/src/components/GasTanks/GasTanks.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Box } from '@chakra-ui/react';
-import { GasTankType } from '../../types';
-
-export default function GasTank(props: GasTankType) {
-    return <Box>{JSON.stringify(props, null, 2)}</Box>;
-}
