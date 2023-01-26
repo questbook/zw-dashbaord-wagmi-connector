0aa4453cac88395f90ada08986212e3baf259b30
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2023-01-26 06:57:52 +0300

    chore: add viewing / adding projects and gas tanks

---
+++ example-dashboard/src/components/Controller/index.tsx
@@ -0,0 +1,32 @@
+import { useContext } from 'react';
+import { Box } from '@chakra-ui/react';
+import { ProjectsContext } from '../../../pages/_app';
+import ProjectViewer from './ProjectViewer';
+import { useAccount, useConnect } from 'wagmi';
+import ProjectCreator from './ProjectCreator';
+import GasTankCreator from './GasTankCreator';
+import GasTankViewer from './GasTankViewer';
+
+interface Props {
+    updateProjects?: () => void;
+}
+
+export default function SmartViewer({ updateProjects }: Props) {
+    const { isConnected } = useAccount()
+    const { selectedEntity, doesScwExist } =
+        useContext(ProjectsContext)!;
+
+    if (!doesScwExist && isConnected) return <Box>Deploying SCW...</Box>;
+    if (!doesScwExist || !selectedEntity) return null;
+
+    if ('gasTanks' in selectedEntity) {
+        return <ProjectViewer {...selectedEntity} />
+    }
+
+    if ('createNew' in selectedEntity) {
+        if (selectedEntity.createNew === 'project') return <ProjectCreator onCreate={updateProjects} />
+        if (selectedEntity.createNew === 'gasTank') return <GasTankCreator projectId={selectedEntity.projectId} onCreate={updateProjects} />
+    }
+
+    return <GasTankViewer {...selectedEntity} />
+}
