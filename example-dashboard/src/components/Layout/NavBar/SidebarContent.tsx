c759ef4b3b71ce9c1495a7a34828a5e1cdf4b253
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2023-01-26 07:04:00 +0300

    chore(sidebar): content loaded from context

--- example-dashboard/src/components/Layout/NavBar/SidebarContent.tsx
+++ example-dashboard/src/components/Layout/NavBar/SidebarContent.tsx
@@ -1,30 +1,25 @@
-import { BoxProps, useColorModeValue, Flex, CloseButton, Box, Text } from "@chakra-ui/react";
-import { GasTankProps } from "./GasTankNavItem";
-import ProjectNavItem from "./ProjectNavItem";
+import { useContext } from 'react';
+import {
+    Box,
+    BoxProps,
+    CloseButton,
+    Flex,
+    Text,
+    useColorModeValue
+} from '@chakra-ui/react';
+import { ProjectsContext } from '../../../../pages/_app';
+import ProjectNavItem from './ProjectNavItem';
+import { useAccount } from 'wagmi';
+import ProjectAdder from './ProjectAdder';
 
 interface SidebarProps extends BoxProps {
     onClose: () => void;
 }
 
-interface ProjectItemProps {
-    name: string;
-    gasTanks: {
-        [key: string]: GasTankProps
-    }
-}
-
-const LinkItems: Array<ProjectItemProps> = [
-    {
-        name: 'Project 1',
-        gasTanks: {
-            '5': {
-                chainId: "5"
-            },
-        },
-    }
-];
-
 const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
+    const { isConnected } = useAccount()
+    const { projects, doesScwExist } = useContext(ProjectsContext)!;
+
     return (
         <Box
             bg={useColorModeValue('white', 'gray.900')}
@@ -33,16 +28,39 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
             w={{ base: 'full', md: 60 }}
             pos="fixed"
             h="full"
-            {...rest}>
-            <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+            {...rest}
+        >
+            <Flex
+                h="20"
+                alignItems="center"
+                mx="8"
+                justifyContent="space-between"
+            >
                 <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
                     Logo
                 </Text>
-                <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+                <CloseButton
+                    display={{ base: 'flex', md: 'none' }}
+                    onClick={onClose}
+                />
             </Flex>
-            {LinkItems.map((link) => (
-                <ProjectNavItem key={link.name} text={link.name} gasTanks={link.gasTanks} />
-            ))}
+            {doesScwExist ? (
+                <>
+                    {projects.sort((a, b) => a.name < b.name ? -1 : 1).map((project) => (
+                        <ProjectNavItem
+                            key={project.name}
+                            project={project}
+                        />
+                    ))}
+
+                    <ProjectAdder />
+                </>
+
+            ) : (
+                isConnected ? (
+                    <Text>Deploying SCW...</Text>) : <Text textAlign="center" color="red.500">
+                    Please connect zero wallet </Text>
+            )}
         </Box>
     );
 };
