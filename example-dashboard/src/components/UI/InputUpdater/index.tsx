f28fbee2cae17bf72075038f14096d2b69980634
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2023-01-29 19:44:24 +0400

    chore(ui): add input with updater

---
+++ example-dashboard/src/components/UI/InputUpdater/index.tsx
@@ -0,0 +1,52 @@
+
+import { CheckIcon, CloseIcon, EditIcon, DeleteIcon } from "@chakra-ui/icons";
+import { Flex, ButtonGroup, Button, Icon, FlexProps, Editable, EditableInput, EditablePreview, useEditableControls, IconButton, Input } from "@chakra-ui/react";
+import { FiTrash2, FiEdit2 } from "react-icons/fi";
+
+interface Props {
+    handleChange: (newVal: string) => void;
+    handleRemove?: () => void;
+    value: string;
+}
+
+function EditableControls({ handleRemove }: { handleRemove?: () => void }) {
+    const {
+        isEditing,
+        getSubmitButtonProps,
+        getCancelButtonProps,
+        getEditButtonProps,
+    } = useEditableControls()
+
+    return isEditing ? (
+        <ButtonGroup justifyContent='center' size='sm' m='2' mr='auto' gap={5}>
+            <IconButton icon={<CheckIcon />} aria-label={''} {...getSubmitButtonProps()} />
+            <IconButton icon={<CloseIcon />} aria-label={''} {...getCancelButtonProps()} />
+        </ButtonGroup>
+    ) : (
+        <ButtonGroup justifyContent='center' size='sm' m='2' mr='auto' gap={5}>
+            <IconButton size='sm' icon={<EditIcon />} aria-label={''} {...getEditButtonProps()} />
+            {
+                !!handleRemove && <IconButton size='sm' icon={<DeleteIcon />} onClick={() => handleRemove()} aria-label={"remove"} />
+            }
+        </ButtonGroup>
+    )
+}
+
+export default function InputUpdater({ handleChange, handleRemove, value, ...rest }: Props & FlexProps) {
+    return (
+        <Flex gap={5} {...rest}>
+            <Editable
+                defaultValue={value}
+                onChange={(newVal) => handleChange(newVal)}
+                textAlign='center'
+                isPreviewFocusable={false}
+                display='flex'
+                alignItems='center'
+            >
+                <EditablePreview w='sm' />
+                <Input as={EditableInput} w='sm' />
+                <EditableControls handleRemove={handleRemove} />
+            </Editable>
+        </Flex>
+    )
+}
